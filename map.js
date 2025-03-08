e8f4a5acf1e818aeb38dba24692bd182ce6a8900
llindros <166050787+llindros@users.noreply.github.com>
2025-03-08 10:13:52 -0800

    rebuilding

---
+++ map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibGlseWxpbmQiLCJhIjoiY2x3amdzanBmMTJidzJqbWpnYzlkZnAyZyJ9.csn_7z53ZtqByVzYMZRVuA';
+    
+    
+    var map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/lilylind/clwjhkej600r601q1hdwj8vjw', //style link
+        center: [0,0], // starting position
+        zoom: 1, // starting zoom
+    });
+
