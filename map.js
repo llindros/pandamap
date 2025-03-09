53ab07dda7c361bca4f2bfd3cf85e713d09de061
llindros <166050787+llindros@users.noreply.github.com>
2025-03-09 01:40:54 -0800

    fixing top title bar

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibGlseWxpbmQiLCJhIjoiY2x3amdzanBmMTJidzJqbWpnYzlkZnAyZyJ9.csn_7z53ZtqByVzYMZRVuA';
     
     
-    var map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/lilylind/clwjhkej600r601q1hdwj8vjw', //style link
         center: [0,0], // starting position
-        zoom: 1, // starting zoom
+        zoom: 1.3, // starting zoom
+        FontFaceSet: ['Lato']
     });
 
